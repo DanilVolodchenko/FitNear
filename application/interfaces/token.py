8e2afc91c82f58de22a3fa27c7c4403dfae18e2a
Danil Volodchenko <danil.volodchenko@inbox.ru>
2026-07-06 20:55:36 +0700

    Add token reader, saver and updater

--- application/interfaces/token.py
+++ application/interfaces/token.py
@@ -1,13 +1,22 @@
 import abc
 
+from application.dto.token import CreateRegisterTokenDTO
+from domain.entities.token import RegistrationTokenDM
 
-class ITokenReader(abc.ABC):
+
+class IRegistrationTokenReader(abc.ABC):
     @abc.abstractmethod
-    async def get_by_(self):
-        ...
+    async def get_by_token_hash(self, token_hash: str) -> RegistrationTokenDM | None:
+        """Get registration token by token_hash."""
 
 
-class ITokenSaver(abc.ABC):
+class IRegistrationTokenSaver(abc.ABC):
     @abc.abstractmethod
-    async def create(self) -> None:
-        """"""
+    async def create(self, token_dto: CreateRegisterTokenDTO) -> RegistrationTokenDM:
+        """Create registration token."""
+
+
+class IRegistrationTokenDeactivator(abc.ABC):
+    @abc.abstractmethod
+    async def deactivate_by_user(self, user_id: int) -> None:
+        """Deactivate all token by user_id."""
