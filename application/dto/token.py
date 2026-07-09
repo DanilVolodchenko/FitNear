35c96eb619b67c6767f95ad72812d8e94ddc75a8
Danil Volodchenko <danil.volodchenko@inbox.ru>
2026-07-09 21:15:46 +0700

    Create token dto

---
+++ application/dto/token.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass
+from datetime import datetime
+
+from domain.entities.token import RegistrationTokenType
+
+
+@dataclass(slots=True)
+class CreateRegisterTokenDTO:
+    user_id: int
+    token_hash: str
+    type: RegistrationTokenType
+    expires_at: datetime
+    active: bool = True
