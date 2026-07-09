237c20b437476dd91f5b3c5654806019063175b6
Danil Volodchenko <danil.volodchenko@inbox.ru>
2026-07-09 21:30:52 +0700

    Modify main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import uvicorn
 
-from core.registrar import register_app
 from core.config import config
+from core.registrar import register_app
+
+app = register_app(config.fastapi)
 
 if __name__ == '__main__':
-    app = register_app(config.fastapi)
-
-    uvicorn.run(app)
+    uvicorn.run('main:app', reload=True)
