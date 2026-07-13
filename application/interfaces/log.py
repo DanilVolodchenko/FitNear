414702a2a1c2c42df0e6378b411b0dafa3ef7c3d
danil <da.volodchenko@sm117.ru>
2026-07-13 15:54:51 +0700

    Add logger interface

---
+++ application/interfaces/log.py
@@ -0,0 +1,32 @@
+import abc
+from typing import Any
+
+
+class ILogger(abc.ABC):
+    @abc.abstractmethod
+    def trace(self, message: str, *args: Any, **kwargs: Any) -> None:
+        """Trace level."""
+
+    @abc.abstractmethod
+    def debug(self, message: str, *args: Any, **kwargs: Any) -> None:
+        """Debug level."""
+
+    @abc.abstractmethod
+    def info(self, message: str, *args: Any, **kwargs: Any) -> None:
+        """Info level."""
+
+    @abc.abstractmethod
+    def success(self, message: str, *args: Any, **kwargs: Any) -> None:
+        """Success level."""
+
+    @abc.abstractmethod
+    def warning(self, message: str, *args: Any, **kwargs: Any) -> None:
+        """Warning level."""
+
+    @abc.abstractmethod
+    def error(self, message: str, *args: Any, **kwargs: Any) -> None:
+        """Error level."""
+
+    @abc.abstractmethod
+    def critical(self, message: str, *args: Any, **kwargs: Any) -> None:
+        """Critical level."""
