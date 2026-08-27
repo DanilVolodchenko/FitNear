a92e43f8f59de6ee1489917e2ec7f59b18c04f49
danil <da.volodchenko@sm117.ru>
2026-08-27 10:01:28 +0700

    Add func to create database if it does not exists

--- src/infrastructure/resources/database.py
+++ src/infrastructure/resources/database.py
@@ -1,6 +1,24 @@
+import psycopg
+
+from psycopg import sql
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
 from config import PostgresConfig
+
+
+async def create_db_if_not_exists(psql_config: PostgresConfig) -> None:
+    conn_info = f'dbname=postgres host={psql_config.host} port={psql_config.port} user={psql_config.username} password={psql_config.pwd}'
+
+    async with await psycopg.AsyncConnection.connect(conn_info, autocommit=True) as connection:
+        async with connection.cursor() as cursor:
+            check_db_stmt = sql.SQL('SELECT 1 FROM pg_database WHERE datname = %s')
+
+            await cursor.execute(check_db_stmt, (psql_config.db,))
+            exists = await cursor.fetchone()
+
+            if not exists:
+                create_db_stmt = sql.SQL('CREATE DATABASE {}').format(sql.Identifier(psql_config.db))
+                await cursor.execute(create_db_stmt)
 
 
 def new_session_maker(psql_config: PostgresConfig) -> async_sessionmaker[AsyncSession]:
