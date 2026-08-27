b28bd4b70ccf97ea3585f3e08f55fbeb14f05376
danil <da.volodchenko@sm117.ru>
2026-08-27 10:02:06 +0700

    Create database if exists before create app

--- registrar/lifespan.py
+++ registrar/lifespan.py
@@ -3,13 +3,19 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 
+from config.config import Config
 from config.config_path import I18N_PATH
 from di import ioc
 from src.infrastructure.localization import ITranslator
+from src.infrastructure.resources.database import create_db_if_not_exists
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI) -> AsyncGenerator[None]:
+    config = await ioc.get(Config)
+
+    await create_db_if_not_exists(config.postgres)
+
     translator = await ioc.get(ITranslator)
     translator.compile(I18N_PATH)
 
