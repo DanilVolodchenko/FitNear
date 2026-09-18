f4b72ff0a20f5a6c45d694ba73cbd90249761615
danil <da.volodchenko@sm117.ru>
2026-09-18 09:29:11 +0700

    Add alembic-postgresql-enum dependency

--- src/infrastructure/models/migrations/env.py
+++ src/infrastructure/models/migrations/env.py
@@ -1,5 +1,7 @@
 from logging.config import fileConfig
 
+# Issue: https://github.com/sqlalchemy/alembic/issues/886
+import alembic_postgresql_enum  # noqa
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 
